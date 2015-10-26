bdf82a0a2f38985b569d6e880aa9086a22bdec55
ssethubalan <ssethubalan@logitech.com>
2015-10-26 12:10:57 -0700

    test commit

--- TestProject/src/com/test/Test.java
+++ TestProject/src/com/test/Test.java
@@ -3,5 +3,8 @@ package com.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello world develop");
+		String str = "Something to Test";
+		
+		System.out.println("str: "+ str.length());
 	}
 }
