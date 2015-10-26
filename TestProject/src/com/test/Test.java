2a30ca67c7c2fe1d5bc5377e19013d7550ffd099
srajagop <santhana_rajagopalan@hotmail.com>
2015-10-26 12:41:05 -0700

    After release code

--- TestProject/src/com/test/Test.java
+++ TestProject/src/com/test/Test.java
@@ -3,5 +3,7 @@ package com.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello world develop");
+		String str = "Something big";
+		System.out.println("str: "+ str);
 	}
 }
